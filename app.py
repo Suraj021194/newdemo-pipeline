a037af5956d6e8a0baeb96a1b4f47a3833d960b8
Suraj021194 <surajsrivastava021194@gmail.com>
2025-12-28 00:49:32 +0530

    Modified few lines in dockerfile, deployment.yaml and requirement.txt

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+from flask import Flask
+
 
 def main():
     version = os.getenv("APP_VERSION", "dev")
@@ -6,5 +8,19 @@ def main():
     print(f"Deploying version {version}")
     print(f"Using secret key: {api_key}")
 
+# Flask app for service mode
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    version = os.getenv("APP_VERSION", "dev")
+    api_key = os.getenv("API_KEY", "no-key")
+    return f"Deploying version {version}, Using secret key: {api_key}"
+
 if __name__ == "__main__":
-    main()
+    # Decide mode based on ENV variable
+    mode = os.getenv("RUN_MODE", "job")
+    if mode == "service":
+        app.run(host="0.0.0.0", port=5000)
+    else:
+        main()
